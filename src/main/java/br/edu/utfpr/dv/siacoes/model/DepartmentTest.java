39a490deb8c64b0a7052990125271c039f1531e3
Stefanie Fernandes <stefanie.cristina@ykt.com.br>
2020-09-16 00:02:44 -0300

    classe de teste junit

---
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/DepartmentTest.java
@@ -0,0 +1,70 @@
+package br.edu.utfpr.dv.siacoes.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentTest {
+
+    @org.junit.jupiter.api.Test
+    void getIdDepartment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCampus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFullName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLogo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isActive() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSite() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getInitials() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setIdDepartment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCampus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setFullName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setLogo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setActive() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSite() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setInitials() {
+    }
+}
