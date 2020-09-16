1f612b17d2d4ee3637dc8760df4f9fd948400baf
Stefanie Fernandes <stefanie.cristina@ykt.com.br>
2020-09-15 23:20:14 -0300

    criando classe abstrata para servir como classe base e aplicar o template method

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/TemplateDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/TemplateDAO.java
@@ -1,2 +1,19 @@
-package br.edu.utfpr.dv.siacoes.dao;public class TemplateDAO {
+package br.edu.utfpr.dv.siacoes.dao;
+
+import java.util.List ;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+
+public abstract class TemplateDAO<T> {
+    public abstract List <T> listAll() throws SQLException;
+
+    public abstract T findById(int id) throws SQLException;
+
+    public abstract T loadObject(ResultSet rs) throws SQLException;
+
+    public abstract save(int idUser, T unit) throws SQLException;
+
 }
